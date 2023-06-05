ba597477599091ac4eebd4d34bf8395a1ff7c7e4
vanshajbajaj <78155493+vanshajbajaj@users.noreply.github.com>
2023-06-05 23:06:09 +0530

    Add files via upload

--- server/router/auth.js
+++ server/router/auth.js
@@ -10,6 +10,8 @@ const ObjectId = require('mongodb').ObjectId;
 
 require("./../config/mongoose");
 
+router.use(cors());
+
 const User = require("./../models/User");
 const Post = require("./../models/Post");
 const authenticate = require('../middleware/authenticate');
